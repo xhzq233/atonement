aa85758f7cd40e91eca77186cc4be61bf974af82
shinxiahou <xhzq233@qq.com>
2024-06-28 01:30:17 +0800

    Add push

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:http/http.dart' as http;
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 CollectionReference<Map<String, dynamic>> message = firestore.collection('messages');
@@ -21,24 +22,56 @@ GoogleSignIn _googleSignIn = GoogleSignIn(
 final Rx<GoogleSignInAccount?> _currentUser = null.obs;
 final Rx<String?> _fcmToken = null.obs;
 
-Future<void> _new(String content) {
+// POST https://fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send HTTP/1.1
+//
+// Content-Type: application/json
+// Authorization: Bearer ya29.ElqKBGN2Ri_Uz...HnS_uNreA
+// {
+//   "message":{
+//     "topic" : "foo-bar",
+//     "notification" : {
+//       "body" : "This is a Firebase Cloud Messaging Topic Message!",
+//       "title" : "FCM Message"
+//       }
+//    }
+// }
+Future<void> _newMessage(String content) async {
   if (content.isEmpty) {
     SmartDialog.showToast('Content is empty');
     return Future.value();
   }
-  // Call the user's CollectionReference to add a new user
-  return message.add({
-    'content': content,
-    'time': DateTime.now().millisecondsSinceEpoch,
-    'send': _currentUser.value!.displayName,
-    'read': 0,
-  }).then((DocumentReference doc) {
+  try {
+    final DocumentReference doc = await message.add({
+      'content': content,
+      'time': DateTime.now().millisecondsSinceEpoch,
+      'send': _currentUser.value!.displayName,
+      'read': 0,
+    });
     print('Message added with ID: ${doc.id}');
     SmartDialog.showToast('Message added');
-  }).catchError((error) {
-    print("Failed to add messasge: $error");
-    SmartDialog.showToast(error.toString());
-  });
+
+    // Send http request to FCM
+    http.post(
+      Uri.parse('https://fcm.googleapis.com/v1/projects/xhzq233-firebase-demo/messages:send'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization':
+            'Bearer ya29.c.c0AY_VpZjji8FycSHT6gXh8GpjKZoTWGQCYLmelYz1vwKWM3niK-DB48GHd6EgkOepJNM4RZN9GGaYDyvQ03tRcQi6iNLHUiEhFXB7AVy3qk-ncqSIGoKpzymYPh-FC6AyzJh4SEaqm3xqCv7G6ICNFtV_-Zuvz8X0b3IrOg5RQdeHhTbxeCqM8zapZKIusnSqLyPRizZLLI-3DU7k3W6o55gDHGJJhUCPgkHv-esMJyl3aNUATD3Dww3PRoNh0QIsNSmNmCcWWB88s9qFOeviIIjRnjZwX3B8UQs0x0L21dIYrfGuAUcmYUPsIUTnTtXb7bCdA5NykxN1gXmT-Ox32RA4dTtvAiIRhUqFwGxe1qR3WygAiH0aUYYE384CldjgdQM8w7wimcZ28u595UiMhe4yh-RZRqmwaht6xMgWshhI1c0tQ6nex2Zg5Qjo4Sh8Y_dwsnZSd-sIO6gon4khYskgwuI-_9sVwo3fUyJ49eemUnMtaro5kaksBcZvWi1j01Mou44Qv7RFygpvpMU3xuMon8WsFyF3ZsZvXX7O-agQq3Uw74RkpSY9X_y7FxbJWv2F64x0MR_kRzgIJihWQ_Ucu2tdsVY_J3BalybtYBM6lQ8x_7US3dvoMybofpk-lo0qW8r4jsykleUlgQlmhUsR_Q-m0f_z8dkBmRJS7_WJYv1X6YM38Q6ym6SU47tvUpZ5WO-dJqkWhcpVcXRevgI_ZrJ5FzcRqRbB5FFg-b4l0F3g4I9UUSQbhias_MdV-lW3sZh2M5h43VVOnYOmspyams9_atQxex7i1nidQFRS4aIzB0MWIjJbiryYvnQVju2s-y1X_fpuhnOiergXRiqwkwm7j-k2oRw0kxlrjZcBfS_YBhORkJXjzs1qRw1_piQXh93l_0ye8Yyg07M9xbSB1vujqdfs3JJga6YMZk_8kWlY6qynhsO-u8k0kzzag7bcV_h4vcS2XrXoyYe7fdWhRe2IorJdeIObkU9QeyQZiB3BQX3aq7a',
+      },
+      body: <String, dynamic>{
+        'message': <String, dynamic>{
+          'topic': 'message',
+          'notification': <String, dynamic>{
+            'body': content,
+            'title': 'New message',
+          },
+        },
+      },
+    );
+  } catch (e) {
+    print(e.toString());
+    SmartDialog.showToast(e.toString());
+  }
 }
 
 Future<void> _handleSignIn() async {
@@ -56,16 +89,16 @@ void main() {
   () async {
     final signedIn = await _googleSignIn.isSignedIn();
 
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) => _currentUser.value = account);
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      _currentUser.value = account;
+    });
     if (_googleSignIn.currentUser != null && signedIn) {
       _currentUser.value = _googleSignIn.currentUser;
     } else {
       _googleSignIn.signInSilently();
     }
 
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
     NotificationSettings settings = await FirebaseMessaging.instance.getNotificationSettings();
 
     if (settings.authorizationStatus == AuthorizationStatus.notDetermined) {
@@ -82,10 +115,12 @@ void main() {
       print('User granted permission: ${settings.authorizationStatus}');
     }
 
+    SmartDialog.showToast('User granted permission: ${settings.authorizationStatus}');
+
     try {
       _fcmToken.value = await FirebaseMessaging.instance.getToken(
           vapidKey: "BJOH1yndL3f6ZACCOjd20QpM8SNpSdWDAZMKSsMLWMdnivi_9hBeIgzCkvjWhXSrM76M1B561lZ7dHrcEf1zpig");
-      FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
         alert: true,
         badge: true,
         sound: true,
@@ -97,12 +132,17 @@ void main() {
       });
 
       FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-        print("onMessage: $message");
+        print('Got a message whilst in the foreground!');
+        print('Message data: ${message.data}');
 
-        SmartDialog.showToast(message.notification?.title ?? 'New message');
+        if (message.notification != null) {
+          print('Message also contained a notification: ${message.notification}');
+          SmartDialog.showToast(message.notification?.title ?? 'New message');
+        }
       });
     } catch (e) {
       print(e.toString());
+      SmartDialog.showToast(e.toString());
     }
   }();
 }
@@ -229,7 +269,7 @@ class _TextField extends GetView<_TextFieldViewModel> {
         ),
         Obx(
           () => CupertinoButton(
-            onPressed: _currentUser.value == null ? null : () => _new(vm.controller.text),
+            onPressed: _currentUser.value == null ? null : () => _newMessage(vm.controller.text),
             child: const Text('发布'),
           ),
         ),
