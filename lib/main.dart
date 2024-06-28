8480df61cbe8ed7fdeea259f80e579f10ffa0a4a
shinxiahou <shinxiahou@tencent.com>
2024-06-28 16:51:48 +0800

    Add backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,150 +1,18 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-
-import 'firebase_options.dart';
 
 import 'package:atonement/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:http/http.dart' as http;
 
-FirebaseFirestore firestore = FirebaseFirestore.instance;
-CollectionReference<Map<String, dynamic>> message = firestore.collection('messages');
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  clientId: '80750108764-kfjd619q2e4cchruateg3uv0j32ncnrc.apps.googleusercontent.com',
-  scopes: ['email'],
-);
-final Rx<GoogleSignInAccount?> _currentUser = null.obs;
-final Rx<String?> _fcmToken = null.obs;
-
-// POST https://fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send HTTP/1.1
-//
-// Content-Type: application/json
-// Authorization: Bearer ya29.ElqKBGN2Ri_Uz...HnS_uNreA
-// {
-//   "message":{
-//     "topic" : "foo-bar",
-//     "notification" : {
-//       "body" : "This is a Firebase Cloud Messaging Topic Message!",
-//       "title" : "FCM Message"
-//       }
-//    }
-// }
-Future<void> _newMessage(String content) async {
-  if (content.isEmpty) {
-    SmartDialog.showToast('Content is empty');
-    return Future.value();
-  }
-  try {
-    final DocumentReference doc = await message.add({
-      'content': content,
-      'time': DateTime.now().millisecondsSinceEpoch,
-      'send': _currentUser.value!.displayName,
-      'read': 0,
-    });
-    print('Message added with ID: ${doc.id}');
-    SmartDialog.showToast('Message added');
-
-    // Send http request to FCM
-    http.post(
-      Uri.parse('https://fcm.googleapis.com/v1/projects/xhzq233-firebase-demo/messages:send'),
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-        'Authorization':
-            'Bearer ya29.c.c0AY_VpZjji8FycSHT6gXh8GpjKZoTWGQCYLmelYz1vwKWM3niK-DB48GHd6EgkOepJNM4RZN9GGaYDyvQ03tRcQi6iNLHUiEhFXB7AVy3qk-ncqSIGoKpzymYPh-FC6AyzJh4SEaqm3xqCv7G6ICNFtV_-Zuvz8X0b3IrOg5RQdeHhTbxeCqM8zapZKIusnSqLyPRizZLLI-3DU7k3W6o55gDHGJJhUCPgkHv-esMJyl3aNUATD3Dww3PRoNh0QIsNSmNmCcWWB88s9qFOeviIIjRnjZwX3B8UQs0x0L21dIYrfGuAUcmYUPsIUTnTtXb7bCdA5NykxN1gXmT-Ox32RA4dTtvAiIRhUqFwGxe1qR3WygAiH0aUYYE384CldjgdQM8w7wimcZ28u595UiMhe4yh-RZRqmwaht6xMgWshhI1c0tQ6nex2Zg5Qjo4Sh8Y_dwsnZSd-sIO6gon4khYskgwuI-_9sVwo3fUyJ49eemUnMtaro5kaksBcZvWi1j01Mou44Qv7RFygpvpMU3xuMon8WsFyF3ZsZvXX7O-agQq3Uw74RkpSY9X_y7FxbJWv2F64x0MR_kRzgIJihWQ_Ucu2tdsVY_J3BalybtYBM6lQ8x_7US3dvoMybofpk-lo0qW8r4jsykleUlgQlmhUsR_Q-m0f_z8dkBmRJS7_WJYv1X6YM38Q6ym6SU47tvUpZ5WO-dJqkWhcpVcXRevgI_ZrJ5FzcRqRbB5FFg-b4l0F3g4I9UUSQbhias_MdV-lW3sZh2M5h43VVOnYOmspyams9_atQxex7i1nidQFRS4aIzB0MWIjJbiryYvnQVju2s-y1X_fpuhnOiergXRiqwkwm7j-k2oRw0kxlrjZcBfS_YBhORkJXjzs1qRw1_piQXh93l_0ye8Yyg07M9xbSB1vujqdfs3JJga6YMZk_8kWlY6qynhsO-u8k0kzzag7bcV_h4vcS2XrXoyYe7fdWhRe2IorJdeIObkU9QeyQZiB3BQX3aq7a',
-      },
-      body: <String, dynamic>{
-        'message': <String, dynamic>{
-          'topic': 'message',
-          'notification': <String, dynamic>{
-            'body': content,
-            'title': 'New message',
-          },
-        },
-      },
-    );
-  } catch (e) {
-    print(e.toString());
-    SmartDialog.showToast(e.toString());
-  }
-}
-
-Future<void> _handleSignIn() async {
-  try {
-    await _googleSignIn.signIn();
-  } catch (error) {
-    print(error);
-    SmartDialog.showToast(error.toString());
-  }
-}
+import 'messaging.dart';
 
 void main() {
   runApp(const MyApp());
 
-  () async {
-    final signedIn = await _googleSignIn.isSignedIn();
-
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
-      _currentUser.value = account;
-    });
-    if (_googleSignIn.currentUser != null && signedIn) {
-      _currentUser.value = _googleSignIn.currentUser;
-    } else {
-      _googleSignIn.signInSilently();
-    }
-
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-    NotificationSettings settings = await FirebaseMessaging.instance.getNotificationSettings();
-
-    if (settings.authorizationStatus == AuthorizationStatus.notDetermined) {
-      NotificationSettings settings = await FirebaseMessaging.instance.requestPermission(
-        alert: true,
-        announcement: false,
-        badge: true,
-        carPlay: false,
-        criticalAlert: false,
-        provisional: false,
-        sound: true,
-      );
-
-      print('User granted permission: ${settings.authorizationStatus}');
-    }
-
-    SmartDialog.showToast('User granted permission: ${settings.authorizationStatus}');
-
-    try {
-      _fcmToken.value = await FirebaseMessaging.instance.getToken(
-          vapidKey: "BJOH1yndL3f6ZACCOjd20QpM8SNpSdWDAZMKSsMLWMdnivi_9hBeIgzCkvjWhXSrM76M1B561lZ7dHrcEf1zpig");
-      await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-        alert: true,
-        badge: true,
-        sound: true,
-      );
-
-      FirebaseMessaging.instance.onTokenRefresh.listen((String token) {
-        print('Token refreshed: $token');
-        _fcmToken.value = token;
-      });
-
-      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-        print('Got a message whilst in the foreground!');
-        print('Message data: ${message.data}');
-
-        if (message.notification != null) {
-          print('Message also contained a notification: ${message.notification}');
-          SmartDialog.showToast(message.notification?.title ?? 'New message');
-        }
-      });
-    } catch (e) {
-      print(e.toString());
-      SmartDialog.showToast(e.toString());
-    }
-  }();
+  initFirebase();
 }
 
 class MyApp extends StatelessWidget {
@@ -219,22 +87,22 @@ class _Drawer extends StatelessWidget {
           CupertinoListTile.notched(
             title: const Text('Google 登录'),
             leading: Obx(() => CircleAvatar(
-                  backgroundImage: NetworkImage(_currentUser.value?.photoUrl ?? ''),
+                  backgroundImage: NetworkImage(currentUser.value?.photoUrl ?? ''),
                 )),
-            additionalInfo: Obx(() => Text(_currentUser.value?.displayName ?? '未登录')),
+            additionalInfo: Obx(() => Text(currentUser.value?.displayName ?? '未登录')),
             trailing: const CupertinoListTileChevron(),
-            onTap: _handleSignIn,
+            onTap: handleSignIn,
           ),
           CupertinoListTile.notched(
             title: const Text('FCM Token'),
             leading: const Icon(CupertinoIcons.bell, color: CupertinoColors.systemYellow),
-            trailing: const Icon(CupertinoIcons.doc_on_doc, color: CupertinoColors.systemBlue),
-            onTap: () => Clipboard.setData(ClipboardData(text: _fcmToken.value.toString())),
+            trailing: const Icon(CupertinoIcons.doc_on_doc, color: CupertinoColors.systemYellow),
+            onTap: () => Clipboard.setData(ClipboardData(text: fcmToken.value.toString())),
           ),
-          CupertinoListTile.notched(
-            title: const Text('登出'),
-            leading: const Icon(CupertinoIcons.power, color: CupertinoColors.systemRed),
-            onTap: _googleSignIn.signOut,
+          const CupertinoListTile.notched(
+            title: Text('登出'),
+            leading: Icon(CupertinoIcons.power, color: CupertinoColors.systemRed),
+            onTap: handleSignOut,
           ),
         ],
       ),
@@ -269,7 +137,7 @@ class _TextField extends GetView<_TextFieldViewModel> {
         ),
         Obx(
           () => CupertinoButton(
-            onPressed: _currentUser.value == null ? null : () => _newMessage(vm.controller.text),
+            onPressed: currentUser.value == null ? null : () => pushMessage(vm.controller.text),
             child: const Text('发布'),
           ),
         ),
@@ -288,7 +156,7 @@ class _Posts extends StatelessWidget {
         middle: Text('记录'),
       ),
       child: StreamBuilder<QuerySnapshot>(
-        stream: message.orderBy('time', descending: true).snapshots(),
+        stream: messageSource,
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) {
             return Align(child: Text('Something went wrong ${snapshot.error}'));
@@ -307,7 +175,6 @@ class _Posts extends StatelessWidget {
           return ListView(
             children: snapshot.data!.docs.map((DocumentSnapshot document) {
               Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-              print(data);
               return CupertinoListTile(
                 title: Text(data['content']),
                 subtitle: Text(DateTime.fromMillisecondsSinceEpoch(data['time']).toIso8601String()),
---
+++ lib/messaging.dart
@@ -0,0 +1,195 @@
+/// atonement - messaging
+/// Created by xhz on 6/28/24
+library;
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:get/get.dart';
+import 'dart:convert';
+
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
+
+import 'firebase_options.dart';
+
+FirebaseFirestore _firestore = FirebaseFirestore.instance;
+CollectionReference<Map<String, dynamic>> _messageDb = _firestore.collection('messages');
+
+Stream<QuerySnapshot<Map<String, dynamic>>> get messageSource =>
+    _messageDb.orderBy('time', descending: true).snapshots();
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  clientId: '80750108764-kfjd619q2e4cchruateg3uv0j32ncnrc.apps.googleusercontent.com',
+  scopes: ['email'],
+);
+final Rx<GoogleSignInAccount?> currentUser = null.obs;
+final Rx<String?> fcmToken = null.obs;
+
+void initFirebase() async {
+  final signedIn = await _googleSignIn.isSignedIn();
+
+  _googleSignIn.onCurrentUserChanged.listen(handleAccount);
+  if (_googleSignIn.currentUser != null && signedIn) {
+    handleAccount(_googleSignIn.currentUser);
+  } else {
+    _googleSignIn.signInSilently();
+  }
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  NotificationSettings settings = await FirebaseMessaging.instance.getNotificationSettings();
+
+  if (settings.authorizationStatus == AuthorizationStatus.notDetermined) {
+    settings = await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+  }
+
+  print('User granted permission: ${settings.authorizationStatus}');
+  SmartDialog.showToast('User granted permission: ${settings.authorizationStatus}');
+
+  try {
+    fcmToken.value = await FirebaseMessaging.instance
+        .getToken(vapidKey: "BJOH1yndL3f6ZACCOjd20QpM8SNpSdWDAZMKSsMLWMdnivi_9hBeIgzCkvjWhXSrM76M1B561lZ7dHrcEf1zpig");
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    FirebaseMessaging.instance.onTokenRefresh.listen((String token) {
+      print('Token refreshed: $token');
+      fcmToken.value = token;
+      _pushToken(token);
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+        SmartDialog.showToast(message.notification?.title ?? 'New essage');
+      }
+    });
+  } catch (e) {
+    print(e.toString());
+    SmartDialog.showToast(e.toString());
+  }
+}
+
+Future<void> pushMessage(String content) async {
+  if (content.isEmpty) {
+    SmartDialog.showToast('Content is empty');
+    return Future.value();
+  }
+  try {
+    final DocumentReference doc = await _messageDb.add({
+      'content': content,
+      'time': DateTime.now().millisecondsSinceEpoch,
+      'send': currentUser.value!.displayName,
+      'read': 0,
+    });
+    print('Message added with ID: ${doc.id}');
+    SmartDialog.showToast('Message added');
+
+    final resp = await http.post(
+      Uri.parse('https://at.mar1sa.icu/push'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(
+        <String, dynamic>{
+          'sender': currentUser.value!.displayName,
+          'content': content,
+        },
+      ),
+    );
+    print(resp.body);
+  } catch (e) {
+    print(e.toString());
+    SmartDialog.showToast(e.toString());
+  }
+}
+
+Future<void> _pushToken(String? token) async {
+  if (token == null) {
+    return Future.value();
+  }
+  try {
+    final resp = await http.get(Uri.parse('https://at.mar1sa.icu/pushToken?token=$token'));
+
+    print(resp.body);
+  } catch (e) {
+    print(e.toString());
+    SmartDialog.showToast(e.toString());
+  }
+}
+
+Future<void> handleSignOut() async {
+  try {
+    await _googleSignIn.signOut();
+  } catch (error) {
+    print(error);
+    SmartDialog.showToast(error.toString());
+  }
+}
+
+Future<void> handleSignIn() async {
+  try {
+    await _googleSignIn.signIn();
+  } catch (error) {
+    print(error);
+    SmartDialog.showToast(error.toString());
+  }
+}
+
+void handleAccount(GoogleSignInAccount? account) async {
+  currentUser.value = account;
+  if (account == null) return;
+  try {
+    final GoogleSignInAuthentication signInAuthentication = await account.authentication;
+    print(signInAuthentication.accessToken);
+    // AuthCredential credential = GoogleAuthProvider.credential(
+    //   idToken: signInAuthentication.idToken,
+    //   accessToken: signInAuthentication.accessToken,
+    // );
+    // UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+    // print("user name" + userCredential.user.uid);
+    // this.firebaseUser = userCredential.user;
+    // print("firebaseUser name" + firebaseUser.displayName);
+  } catch (e) {
+    print(e.toString());
+    SmartDialog.showToast(e.toString());
+  }
+}
+
+// It is assumed that all messages contain a data field with the key 'type'
+Future<void> setupInteractedMessage() async {
+  // Get any messages which caused the application to open from
+  // a terminated state.
+  RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+
+  // If the message also contains a data property with a "type" of "chat",
+  // navigate to a chat screen
+  if (initialMessage != null) {
+    _handleMessage(initialMessage);
+  }
+
+  // Also handle any interaction when the app is in the background via a
+  // Stream listener
+  FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+}
+
+void _handleMessage(RemoteMessage message) {
+  if (message.data['path'] == '/posts') {
+    Get.toNamed('/post');
+  }
+}
