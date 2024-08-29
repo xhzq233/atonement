7d010a42a74daf7b34857763f642e11ca92fffd2
shinxiahou <shinxiahou@tencent.com>
2024-08-29 18:05:10 +0800

    Fix text

--- lib/bubble.dart
+++ lib/bubble.dart
@@ -14,10 +14,6 @@ class PostBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     final time = data.time;
 
-    final formattedDate = '${time.year}-${time.month}-${time.day} ${time.hour}:${time.minute}';
-
-    final Widget avatarWidget = NNAvatar(imageUrl: data.avatar);
-
     Widget body = SelectableText(data.content, style: Theme.of(context).textTheme.titleSmall!.apply(fontSizeDelta: 2));
     if (data.imageUrl != null) {
       body = BoxyRow(
@@ -29,33 +25,7 @@ class PostBubble extends StatelessWidget {
       );
     }
 
-    return Container(
-      padding: const EdgeInsets.all(8),
-      margin: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: CupertinoColors.systemGrey6.resolveFrom(context),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          DefaultTextStyle.merge(
-            style: Theme.of(context).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.w800),
-            child: Row(
-              children: [
-                avatarWidget,
-                const SizedBox(width: 8),
-                Text(data.send),
-                const Spacer(),
-                Text(formattedDate, style: TextStyle(color: CupertinoColors.systemGrey.resolveFrom(context))),
-              ],
-            ),
-          ),
-          const SizedBox(height: 8),
-          body
-        ],
-      ),
-    );
+    return _Bubble(time: time, sender: data.send, avatar: data.avatar, child: body);
   }
 }
 
@@ -73,10 +43,6 @@ class TodoBubble extends StatelessWidget {
     final bool complete = data.complete;
     final String? imageUrl = data.imageUrl;
 
-    final formattedDate = '${time.year}-${time.month}-${time.day} ${time.hour}:${time.minute}';
-
-    Widget avatarWidget = NNAvatar(imageUrl: avatar);
-
     Widget body = SelectableText(content, style: Theme.of(context).textTheme.titleSmall!.apply(fontSizeDelta: 2));
     body = BoxyRow(
       children: [
@@ -87,14 +53,36 @@ class TodoBubble extends StatelessWidget {
                     completeTodo(data);
                   }
                 : null),
-        Dominant(
+        Dominant.expanded(
+          flex: 4,
           child: Padding(padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 4), child: body),
         ),
-        if (imageUrl != null) const Spacer(),
-        if (imageUrl != null) WrapImage(imageUrl: imageUrl)
+        if (imageUrl != null)
+          Flexible(
+            flex: 2,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: WrapImage(imageUrl: imageUrl),
+            ),
+          )
       ],
     );
 
+    return _Bubble(time: time, sender: sender, avatar: avatar, child: body);
+  }
+}
+
+class _Bubble extends StatelessWidget {
+  const _Bubble({required this.child, required this.time, required this.sender, required this.avatar});
+
+  final Widget child;
+  final DateTime time;
+  final String sender;
+  final String avatar;
+
+  @override
+  Widget build(BuildContext context) {
+    final formattedDate = '${time.year}-${time.month}-${time.day} ${time.hour}:${time.minute}';
     return Container(
       padding: const EdgeInsets.all(8),
       margin: const EdgeInsets.all(8),
@@ -106,10 +94,10 @@ class TodoBubble extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           DefaultTextStyle.merge(
-            style: Theme.of(context).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.w800),
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.w700),
             child: Row(
               children: [
-                avatarWidget,
+                NNAvatar(imageUrl: avatar),
                 const SizedBox(width: 8),
                 Text(sender),
                 const Spacer(),
@@ -118,7 +106,7 @@ class TodoBubble extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 8),
-          body
+          child
         ],
       ),
     );
