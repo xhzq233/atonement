782712cf04d92c3d53b632570fc20741aa512e6f
Shin Xiahou <xhzq233@qq.com>
2024-09-11 08:08:52 -0500

    Update messaging.dart

--- lib/messaging.dart
+++ lib/messaging.dart
@@ -328,7 +328,7 @@ Future<void> setupInteractedMessage() async {
 
 void _handleMessage(RemoteMessage message) {
   fireLogI('A message caused the application to open: ${message.data}');
-  if (message.data['path'] == '/posts') {
-    rootNavigator.pushNamed('/post');
+  if (message.data['path'] ！= null) {
+    rootNavigator.pushNamed(message.data['path']);
   }
 }
